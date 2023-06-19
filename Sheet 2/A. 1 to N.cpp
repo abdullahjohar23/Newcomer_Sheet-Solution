68a1f272a1cc28b1c7c3d8dd8fefb582ddb30dc9
Abdullah Johar <92917264+abdullahjohar23@users.noreply.github.com>
2023-06-20 01:25:55 +0600

    Add files via upload

---
+++ Sheet 2/A. 1 to N.cpp
@@ -0,0 +1,25 @@
+// Codeforces Handle: Abdullah_Johar
+// Newcomer Sheet 2: A. 1 to N
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
