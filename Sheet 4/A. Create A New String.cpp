5c561b95879b8025b7d560c83184c6763404407a
Abdullah Johar <92917264+abdullahjohar23@users.noreply.github.com>
2023-06-26 12:20:19 +0600

    Add files via upload

---
+++ Sheet 4/A. Create A New String.cpp
@@ -0,0 +1,24 @@
+// Codeforces Handle: Abdullah_Johar
+// Newcomer Sheet 4: A. Create A New String
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    cout << s1.size() << ' ' << s2.size() << endl;
+    cout << s1 << ' ' << s2 << endl;
+
+    return 0;
+}
