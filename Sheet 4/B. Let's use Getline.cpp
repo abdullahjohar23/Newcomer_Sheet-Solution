6748f9c956cb1f3d7f6e295582af0915296006f2
Abdullah Johar <92917264+abdullahjohar23@users.noreply.github.com>
2023-06-27 14:09:12 +0600

    Add files via upload

---
+++ Sheet 4/B. Let's use Getline.cpp
@@ -0,0 +1,23 @@
+// Codeforces Handle: Abdullah_Johar
+// Newcomer Sheet 4: B. Let's use Getline
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    char str[100010];
+    scanf("%[^\\]", str);
+ 
+    printf("%s", str);
+
+    return 0;
+}
